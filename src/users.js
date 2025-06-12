c33ac219a764659c67e506b329d917edb89b9101
Karini Sousa <131774110+Karini2037@users.noreply.github.com>
2025-06-11 21:59:30 -0300

    terminando o projeto em react native

--- src/users.js
+++ src/users.js
@@ -1,21 +1,27 @@
 import { View, StyleSheet, Text, TouchableOpacity} from 'react-native'
+import { db } from './firebaseConnection'
+import { deleteDoc, doc } from 'firebase/firestore';
 
 export function UsersList({data}){
-    function handleDeleteItem() {
-
+    async function handleDeleteItem(){
+        const docRef = doc(db, "users", data.id)
+        await deleteDoc(docRef)
         console.log(data);
+   
+        
     }
     return(
         <View style={styles.container}>
             <Text style={styles.item}>Nome: {data.nome}</Text>
             <Text tyle={styles.item}>Idade: {data.idade}</Text>
             <Text tyle={styles.item}>Cargo: {data.cargo}</Text>
-            <TouchableOpacity onPress={handleDeleteItem}>
-                <Text>Deletar usuário</Text>
+            <TouchableOpacity  style={styles.button} onPress={handleDeleteItem}>
+                <Text style={styles.buttonText}>Deletar usuário</Text>
             </TouchableOpacity>
         </View>
     )
 }
+
 
 //customizando
 
@@ -31,4 +37,19 @@ const styles = StyleSheet.create({
         color: "#000",
         fontSize: 16,
     },
+
+button: {
+backgroundColor: "#B3261E",
+alignSelf: "flex-start",
+padding: 4,
+borderRadius: 4,
+marginTop: 16
+},
+
+buttonText: {
+    color: "#FFF",
+    paddingLeft: 8,
+    paddingRight: 8,
+}
+
 })
